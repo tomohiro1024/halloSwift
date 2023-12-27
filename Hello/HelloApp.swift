d8e92945f58b3e09caecca5819335d2cfd1aa611
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-27 11:54:00 +0900

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by 宮野智宏 on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
